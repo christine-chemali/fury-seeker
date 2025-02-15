7fe966e721557d0b3b189c23ea05ad0098e547bd
christine chemali <christine.chemali@laplateforme.io>
2025-02-15 21:31:51 +0100

    feat: add Monster class with attributes and stats method

--- monster.py
+++ monster.py
@@ -1,1 +1,34 @@
 # Monster Class
+
+
+class Monster:
+    def __init__(self, data):
+        self.name = data["name"]
+        self.species = data["species"]
+        self.health = data["health"]
+        self.level = data["level"]
+        self.attack_types = data["attack_types"]
+        self.weaknesses = data["weaknesses"]
+        self.resistances = data["resistances"]
+        self.description = data["description"]
+        self.rarity = data["rarity"]
+        self.defense = data["defense"]
+        self.speed = data["speed"]
+        self.intelligence = data["intelligence"]
+
+    def get_stats(self):
+        """Return monster stats as a dictionary."""
+        return {
+            "name": self.name,
+            "species": self.species,
+            "health": self.health,
+            "level": self.level,
+            "attack_types": self.attack_types,
+            "weaknesses": self.weaknesses,
+            "resistances": self.resistances,
+            "description": self.description,
+            "rarity": self.rarity,
+            "defense": self.defense,
+            "speed": self.speed,
+            "intelligence": self.intelligence
+        }
