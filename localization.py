74eff4879b7c8732b393d97eeef4b87946f9f1c5
christine chemali <christine.chemali@laplateforme.io>
2025-02-16 12:14:24 +0100

    feat: Add Localization class to handle multilingual translations
    - Implemented the Localization class to manage translations in English and French.
    - Added methods to load translations from JSON files for the specified languages.

--- localization.py
+++ localization.py
@@ -1,2 +1,22 @@
+import json
 
-
+class Localization:
+    def __init__(self, default_language = 'en'):
+        self.current_language = default_language
+        self.translations = {}
+        self.load_translation('en')
+        self.load_translation('fr')
+    
+    def load_translations(self,language):
+        """To load translatinons from Json file for the specified language"""
+        try:
+            with open(f"translations_{language}.json", 'r', encoding = 'utf-8') as file:
+                self.translations[language] = json.load(file)
+                # print(f"loaded translations for {language}: {self.translations[language]}") # debug
+        except FileNotFoundError:
+            # print(f"Translation file for '{language}' not found.")  # debug
+            pass
+        except json.JSONDecodeError:
+            # print(f"Error decoding from the translation file for '{language}'.") # debug
+            pass
+            
